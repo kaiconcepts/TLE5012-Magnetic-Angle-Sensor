43fcf692e5a8af780033b4160a69ba6c20e144c3
Enriquez Garcia Juan Antonio (IFAG DES TCP EDS) <JuanAntonio.EnriquezGarcia@infineon.com>
2020-07-01 11:45:28 +0200

    Added wiced library makefile

---
+++ tle5012-angle.mk
@@ -0,0 +1,20 @@
+#############################################
+# Makefile for CYPRESS WICED WICED 43xxx ####
+#############################################
+# Include library folder in "libraries/drivers/sensors".
+# Add lib to demo or snipped to the makefile
+# variable $(NAME)_COMPONENTS += libraries/drivers/sensors
+
+NAME := Lib_tle5012_angle
+
+#GLOBAL_DEFINES   := HALL_SWITCH_FRAMEWORK=HALL_SWITCH_FRMWK_WICED
+
+GLOBAL_INCLUDES  := src/		\
+					src/util 				  		
+
+$(NAME)_SOURCES  := src/Tle5012b.cpp			\
+					src/Tle5012b_4wire.cpp		\
+					src/Tle5012b_reg.cpp		\	
+					src/util/Tle5012b_SPI.cpp	
+
+VALID_PLATFORMS	 := CYW943907AEVAL1F
